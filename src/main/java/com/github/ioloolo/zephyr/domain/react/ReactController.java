d28a27ce56f8ea151bd27eb9a1eafd4d43a8c65d
Son Daehyeon <sondaehyeon01@gmail.com>
2024-03-29 14:55:31 +0900

    load game url from yaml

--- src/main/java/com/github/ioloolo/zephyr/domain/react/ReactController.java
+++ src/main/java/com/github/ioloolo/zephyr/domain/react/ReactController.java
@@ -1,5 +1,6 @@
 package com.github.ioloolo.zephyr.domain.react;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +8,12 @@ import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class ReactController {
+
+	@Value("${app.server.host}")
+	private String host;
+
+	@Value("${app.server.password}")
+	private String password;
 
 	@RequestMapping(value = {"/", "/{x:[\\w\\-]+}", "/{x:^(?!api$).*$}/**/{y:[\\w\\-]+}"})
 	public String getIndexPage() {
@@ -18,7 +25,7 @@ public class ReactController {
 	public RedirectView getConnectPage() {
 
 		RedirectView redirectView = new RedirectView();
-		redirectView.setUrl("steam://connect/teamzephyr.kro.kr/0on6mB85Esc9ZcUn");
+		redirectView.setUrl("steam://connect/%s/%s".formatted(host, password));
 
 		return redirectView;
 	}
