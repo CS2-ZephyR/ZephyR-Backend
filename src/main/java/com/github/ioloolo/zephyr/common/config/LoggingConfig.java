988596421527c382537fafdecd75ab53dcdef130
Son Daehyeon <sondaehyeon01@gmail.com>
2024-03-30 19:49:36 +0900

    Log user info instead of steam id when user is registered

--- src/main/java/com/github/ioloolo/zephyr/common/config/LoggingConfig.java
+++ src/main/java/com/github/ioloolo/zephyr/common/config/LoggingConfig.java
@@ -1,29 +1,43 @@
 package com.github.ioloolo.zephyr.common.config;
 
+import java.util.Optional;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.github.ioloolo.zephyr.domain.user.data.User;
+import com.github.ioloolo.zephyr.domain.user.repository.UserRepository;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Configuration
+@RequiredArgsConstructor
 public class LoggingConfig implements WebMvcConfigurer {
+
+	private final LoggingInterceptor loggingInterceptor;
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 
-		registry.addInterceptor(new LoggingInterceptor())
+		registry.addInterceptor(loggingInterceptor)
 				.order(1)
 				.addPathPatterns("/**")
-				.excludePathPatterns("/public/**", "/*.png");
+				.excludePathPatterns("/public/**");
 	}
 
+	@Component
+	@RequiredArgsConstructor
 	public static class LoggingInterceptor implements HandlerInterceptor {
+
+		private final UserRepository userRepository;
 
 		@Override
 		public boolean preHandle(
@@ -35,7 +49,9 @@ public class LoggingConfig implements WebMvcConfigurer {
 				return true;
 			}
 
-			log.info("[{}] {} {}", steamId, request.getMethod(), request.getRequestURI());
+			Optional<User> userOptional = userRepository.findBySteamId(Long.parseLong(steamId));
+
+			log.info("[{}] {} {}", userOptional.isPresent() ? userOptional.get().getName() : steamId, request.getMethod(), request.getRequestURI());
 
 			return true;
 		}
